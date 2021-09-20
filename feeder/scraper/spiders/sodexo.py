fb6598dabc75e26c102338b8c0cea453a4a11967
Tomas Strand <tomas@fik1.net>
2021-09-20 15:14:04 +0300

    Spider: Sodexo get data from page

--- feeder/scraper/spiders/sodexo.py
+++ feeder/scraper/spiders/sodexo.py
@@ -15,15 +15,7 @@ class SodexoSpider(scrapy.Spider):
         yield from response.follow_all(restaurant_pages, self.parse_restaurant_link)
 
     def parse_restaurant_link(self, response):
-        json_links = response.css('a::attr(href)').re('.*weekly_json.*')
-        # Keep only one of each link
-        json_links = list(set(json_links))
-        for json_url in json_links:
-            yield scrapy.Request(response.urljoin(json_url), self.parse_json)
-
-    def parse_json(self, response):
-        data = json.loads(response.text)
-        r_name = data['meta']['ref_title']
+        r_name = response.css('div.header-title-wrapper h1 span::text').get()
         chain, _ = Chain.objects.get_or_create(
             name=self.name
         )
@@ -31,26 +23,28 @@ class SodexoSpider(scrapy.Spider):
             chain=chain,
             name=r_name
         )
-        date_re = re.compile(r"(?P<start_d>\d+)\.(?P<start_m>\d+)\.")
-        rm = date_re.findall(data['timeperiod'])
-        start_date = None
-        if rm:
-            day, month = rm[0]
-            year = datetime.now().year
-            start_date = date(year, int(month), int(day))
-        else:
-            return
-        for i, d in enumerate(data['mealdates']):
-            m_date = start_date + timedelta(days=i)
-            Meal.objects.filter(restaurant=restaurant, date=m_date).delete()
-            if len(d['courses']) == 0:
+
+        data = response.css('div#menuviewblock')
+        for w in data.css('li'):
+            date_str = w.css('a::text').get()
+            ma = re.findall(r".+?(?P<date>\d{1,2})\.(?P<month>\d{1,2})", date_str)
+            m_date = date.today()
+            if ma:
+                day, moth = ma[0]
+                m_date = date(m_date.year, int(moth), int(day))
+            else:
                 continue
-            for m_i in d['courses']:
-                m = d['courses'][m_i]
-                f_price = m['price'].split('€')[0].strip() if 'price' in m else '0.0'
+            Meal.objects.filter(restaurant=restaurant, date=m_date).delete()
+
+            link = w.css('a::attr(href)').get()
+            for m in data.css(link).css('div.mealrow'):
+                f_title = m.css('p.meal-name::text').get()
+                f_price = m.css('div.mealprices p::text')[0].get().split(' ')[0]
+                f_diets = m.css('div.mealdietcodes span::text').getall()
                 meal = MealItem()
                 meal['restaurant'] = restaurant
                 meal['date'] = m_date
-                meal['food'] = "{} ({})".format(m['title_fi'], m['dietcodes'] if 'dietcodes' in m else '')
+                meal['food'] = "{} ({})".format(f_title, ", ".join(f_diets))
                 meal['price'] = float(f_price.replace(',', '.'))
                 yield meal
+
